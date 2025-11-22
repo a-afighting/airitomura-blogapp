ce733d9ef3a4d42bfcd54de4f2bba3fc5de4cc8e
戸村愛有 <tomura.airi00@gmail.com>
2025-11-22 19:25:16 +0900

    formを部分テンプレートに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'articles#index' #特に指定がなければindexを表示（get~の代わりになる）
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
