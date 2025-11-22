295556e7fb136e814421318a718ec822283b67b9
戸村愛有 <tomura.airi00@gmail.com>
2025-11-22 21:52:39 +0900

    better errorsを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  tittle     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
     validates :tittle, presence:true
     validates :content, presence:true
